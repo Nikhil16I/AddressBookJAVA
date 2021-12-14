a90023363ed00cf9281bb1684aa8cf44cdc5c1f7
[decoderNikhil] <[Nikhilmit777@gmail.com]>
2021-12-14 17:21:34 +0530

    Adding New Contacts to AddressBook

--- src/com/Addressbook/AddressBookMain.java
+++ src/com/Addressbook/AddressBookMain.java
@@ -1,10 +1,56 @@
 package com.Addressbook;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class AddressBookMain {
-	//Added Welcome message
+	static Scanner input = new Scanner(System.in);
+	static ArrayList<ContactDATA> contactBook = new ArrayList<>();
+			
+			public void AddContact() {
+				System.out.print("Enter First Name = ");
+		  	String FirstName = input.next();
+		  		
+		  		System.out.print("Enter Last Name = ");
+		  	String LastName = input.next();
+		  		
+		  		System.out.print("Enter Address = ");
+			String Add = input.next();
+		  		
+		  		System.out.print("Enter City = ");
+		  	String City = input.next();
+		  		
+		  		System.out.print("Enter State = ");
+		  	String State = input.next();
+		  		
+		  		System.out.print("Enter Zip Code = ");
+		  	int ZipCode = input.nextInt();
+		  		
+		  		System.out.print("Enter Phone Number = ");
+		  	int PhoneNumber = input.nextInt();
+		  		
+		  		System.out.print("Enter E-mail = ");
+		  	String Email = input.next();
+			
+		  ContactDATA contact = new ContactDATA(FirstName , LastName , Add , City , State , ZipCode , PhoneNumber , Email);
+		  		contactBook.add(contact);
+		  		
+		  		System.out.println("Contact added Successfully.");
+			}
+				
+
+		public void DisplayContacts() {
+			System.out.println("Contacts in Address Book");
+				for(ContactDATA person : contactBook) {
+					System.out.println(person.toString());
+				}
+			}
 	
 			public static void main(String[] args) {
 				System.out.println("Welcome to AddressBook");
+				AddressBookMain obj = new AddressBookMain();
+				obj.AddContact();
+				obj.DisplayContacts();
 		}
 
 
--- src/com/Addressbook/ContactDATA.java
+++ src/com/Addressbook/ContactDATA.java
@@ -11,6 +11,11 @@ public class ContactDATA {
 	int ZipCode;
 	int PhoneNumber;
 
+	public ContactDATA(String firstName, String lastName, String add, String city, String state, int zipCode,
+			int phoneNumber, String email) {
+		// TODO Auto-generated constructor stub
+	}
+
 	public void AddressBookEx(String Name, String Surname, String address, String city, String state, int zip,
 			int contact, String mail) {
 
@@ -25,9 +30,80 @@ public class ContactDATA {
 
 	}
 
-	public String toString() {
+	public String tostring() {
 		return String.format(FirstName + "," + LastName + "," + Address + "," + City + "," + State + "," + ZipCode + ","
 				+ PhoneNumber + "," + Email);
 
+	}
+
+	public String getFirstName() {
+		return FirstName;
+	}
+
+	public void setFirstName(String firstName) {
+		FirstName = firstName;
+	}
+
+	public String getLastName() {
+		return LastName;
+	}
+
+	public void setLastName(String lastName) {
+		LastName = lastName;
+	}
+
+	public String getAddress() {
+		return Address;
+	}
+
+	public void setAddress(String address) {
+		Address = address;
+	}
+
+	public String getCity() {
+		return City;
+	}
+
+	public void setCity(String city) {
+		City = city;
+	}
+
+	public String getState() {
+		return State;
+	}
+
+	public void setState(String state) {
+		State = state;
+	}
+
+	public String getEmail() {
+		return Email;
+	}
+
+	public void setEmail(String email) {
+		Email = email;
+	}
+
+	public int getZipCode() {
+		return ZipCode;
+	}
+
+	public void setZipCode(int zipCode) {
+		ZipCode = zipCode;
+	}
+
+	public int getPhoneNumber() {
+		return PhoneNumber;
+	}
+
+	public void setPhoneNumber(int phoneNumber) {
+		PhoneNumber = phoneNumber;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return String.format(getFirstName() + "" + getLastName() + "" + getAddress() + "" + getCity() + "" + getState()
+				+ "" + getEmail() + "" + getZipCode() + "" + getPhoneNumber());
 	}
 }
